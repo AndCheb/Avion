846ea2040cbff3300fffa4a441dbc2fd1b655b32
Andrey Cheban <anceb@yandex.ru>
2024-02-20 12:54:23 +0300

    add swiper and styles for section collection

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,11 @@
 import burger from './module/burger.js';
 import account from './module/account.js';
+import Swiper from 'swiper/bundle';
 
 burger();
 account();
+
+const swiper = new Swiper('.swiper', {
+  slidesPerView: 'auto',
+  spaceBetween: 20,
+});
