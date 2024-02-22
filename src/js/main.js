b3f865b53f3442ce908c46e609039d71f3d1bae8
Andrey Cheban <anceb@yandex.ru>
2024-02-22 11:25:07 +0300

    complete the entire 'idea' section

--- src/js/main.js
+++ src/js/main.js
@@ -5,7 +5,16 @@ import Swiper from 'swiper/bundle';
 burger();
 account();
 
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.collection__swiper', {
   slidesPerView: 'auto',
   spaceBetween: 20,
+});
+
+const fadeSwiper = new Swiper('.idea__swiper', {
+  effect: 'fade',
+
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
 });
