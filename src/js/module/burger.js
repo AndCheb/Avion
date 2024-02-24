70107526f5536419a3c521e34d830e3da5e22ba8
Andrey Cheban <anceb@yandex.ru>
2024-02-24 10:50:35 +0300

    bug fix

--- src/js/module/burger.js
+++ src/js/module/burger.js
@@ -1,6 +1,6 @@
 const body = document.querySelector('.page__body');
 const burger = document.querySelector('.burger');
-const burgerOveraly = document.querySelector('.burger__overlay');
+const burgerOverlay = document.querySelector('.burger__overlay');
 const burgerOpenButton = document.querySelector('.burger__open');
 const burgerCloseButton = document.querySelector('.burger__close');
 const burgerLinks = document.querySelectorAll('.burger__link');
@@ -9,7 +9,7 @@ const burgerOpen = () => {
   body.classList.add('scroll-none');
   burger.classList.add('burger--visible');
   burgerCloseButton.classList.add('burger__close--visible');
-  burgerOveraly.classList.add('burger__overlay--active');
+  burgerOverlay.classList.add('burger__overlay--active');
   burgerLinks.forEach((item) => {
     item.addEventListener('click', () => {
       burgerClose();
@@ -20,7 +20,7 @@ const burgerOpen = () => {
 const burgerClose = () => {
   body.classList.remove('scroll-none');
   burger.classList.remove('burger--visible');
-  burgerOveraly.classList.remove('burger__overlay--active');
+  burgerOverlay.classList.remove('burger__overlay--active');
 };
 
 export default function () {
@@ -32,7 +32,7 @@ export default function () {
     burgerClose();
   });
 
-  burgerOveraly.addEventListener('click', () => {
+  burgerOverlay.addEventListener('click', () => {
     burgerClose();
   });
 }
